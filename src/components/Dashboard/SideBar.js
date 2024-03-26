763db73ef5615fb622bf3d659f47763e42a742ce
Enoch Maonga Mokua <emokua@safaricom.co.ke>
2024-03-26 22:25:09 +0300

    replacing makeStyles with ThemeProvider

--- src/components/Dashboard/SideBar.js
+++ src/components/Dashboard/SideBar.js
@@ -3,114 +3,116 @@ import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import { Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles"; // Import createTheme and ThemeProvider
 import Link from "next/link";
 
-const useStyles = makeStyles((theme) => ({
-  drawerPaper: {
-    border: "none",
+// Define your custom theme
+const theme = createTheme({
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        border: "none",
+      },
+    },
   },
-}));
+});
 
 const Sidebar = () => {
-  const classes = useStyles();
-
   return (
-    <Drawer
-      variant="permanent"
-      anchor="left"
-      classes={{
-        paper: classes.drawerPaper,
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "#8bc34a",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          // marginRight:
-        }}
+    <ThemeProvider theme={theme}> {/* Wrap your component with ThemeProvider */}
+      <Drawer
+        variant="permanent"
+        anchor="left"
       >
-        <List style={{ marginTop: "100px" }}>
-          <ListItem>
-            <Link href="/dashboard" passHref style={{ textDecoration: "none" }}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "white",
-                  padding: "16px",
-                  backgroundColor: "#7cb342",
-                  borderRadius: "15px",
-                }}
-              >
-                Admin Panel
-              </Typography>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="/users" passHref style={{ textDecoration: "none" }}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "white",
-                  padding: "16px",
-                  backgroundColor: "#7cb342",
-                  borderRadius: "15px",
-                }}
-              >
-                User Management
-              </Typography>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="/" passHref style={{ textDecoration: "none" }}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "white",
-                  padding: "16px",
-                  backgroundColor: "#7cb342",
-                  borderRadius: "15px",
-                }}
-              >
-                Home
-              </Typography>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="/view-data" passHref style={{ textDecoration: "none" }}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "white",
-                  padding: "16px",
-                  backgroundColor: "#7cb342",
-                  borderRadius: "15px",
-                }}
-              >
-                View Data
-              </Typography>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="/login" passHref style={{ textDecoration: "none" }}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "white",
-                  padding: "16px",
-                  backgroundColor: "#7cb342",
-                  borderRadius: "15px",
-                }}
-              >
-                Feedback
-              </Typography>
-            </Link>
-          </ListItem>
-        </List>
-      </div>
-    </Drawer>
+        <div
+          style={{
+            backgroundColor: "#8bc34a",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            // marginRight:
+          }}
+        >
+          <List style={{ marginTop: "100px" }}>
+            <ListItem>
+              <Link href="/dashboard" passHref style={{ textDecoration: "none" }}>
+                <Typography
+                  variant="h6"
+                  style={{
+                    color: "white",
+                    padding: "16px",
+                    backgroundColor: "#7cb342",
+                    borderRadius: "15px",
+                  }}
+                >
+                  Admin Panel
+                </Typography>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="/users" passHref style={{ textDecoration: "none" }}>
+                <Typography
+                  variant="h6"
+                  style={{
+                    color: "white",
+                    padding: "16px",
+                    backgroundColor: "#7cb342",
+                    borderRadius: "15px",
+                  }}
+                >
+                  User Management
+                </Typography>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="/" passHref style={{ textDecoration: "none" }}>
+                <Typography
+                  variant="h6"
+                  style={{
+                    color: "white",
+                    padding: "16px",
+                    backgroundColor: "#7cb342",
+                    borderRadius: "15px",
+                  }}
+                >
+                  Home
+                </Typography>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="/view-data" passHref style={{ textDecoration: "none" }}>
+                <Typography
+                  variant="h6"
+                  style={{
+                    color: "white",
+                    padding: "16px",
+                    backgroundColor: "#7cb342",
+                    borderRadius: "15px",
+                  }}
+                >
+                  View Data
+                </Typography>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="/login" passHref style={{ textDecoration: "none" }}>
+                <Typography
+                  variant="h6"
+                  style={{
+                    color: "white",
+                    padding: "16px",
+                    backgroundColor: "#7cb342",
+                    borderRadius: "15px",
+                  }}
+                >
+                  Feedback
+                </Typography>
+              </Link>
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </ThemeProvider>
   );
 };
 
